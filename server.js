dbcaf72cf42eb6c854709513824c8f587f8afdeb
TechClouds207 <139922172+TechClouds207@users.noreply.github.com>
2024-07-04 21:23:18 +0530

    install required packages and created express session

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
-const express = require('express');
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
+import 'dotenv/config';
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import session from 'express-session';
+import path from 'path';
 
+// Initiating socket server
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
+// server static files
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'public')));
 
+// handle socket middlewares
+const sessionMiddleware = session({
+  secret: process.env.SESSION_KEY,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+      sameSite: 'strict', // Add SameSite attribute
+  } 
+});
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(sessionMiddleware);
+io.engine.use(sessionMiddleware);
+
+// Serve homepage template
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
+  const sessionId = socket.request.session.id;
+  console.log(sessionId);
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
@@ -19,7 +45,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+const port = process.env.PORT || 3000;
+httpServer.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
